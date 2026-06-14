e55ad1a92884990b29e690e9a262ed8ec77ddfa4
xolirx <fendihan7765@gmail.com>
2026-06-14 21:30:26 +0300

    Update restore.bat

--- restore.bat
+++ restore.bat
@@ -2,13 +2,6 @@
 title StartAllBack Восстановление
 color 0E
 
-net session >nul 2>&1
-if %errorLevel% neq 0 (
-    echo [*] Запрашиваю права администратора...
-    powershell start-process -verb runas "%~dp0restore_admin.bat"
-    exit /b
-)
-
 powershell -ExecutionPolicy Bypass -File "%~dp0SAB.ps1" -Restore
 echo.
 echo [*] Нажмите любую клавишу...
